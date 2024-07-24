27e550e20956a6aaf60d7fcdddc9610583706191
gtn-74 <gtn.r74@gmail.com>
2024-07-24 22:53:31 +0900

    [backend側？]GraphQL サーバー実行

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require("express");
 var { createHandler } = require("graphql-http/lib/use/express");
 var { buildSchema } = require("graphql");
 
+var { ruruHTML } = require("ruru/server")
+
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
   type Query {
@@ -32,3 +34,9 @@ app.all(
 // サーバーの実行はされるが、これだけだとクエリできてない。なんでだ？
 app.listen(4000);
 console.log("Running a GraphQL API server at http://localhost:4000/graphql")
+
+// Serve the GraphiQL IDE.
+app.get("/", (_req, res) => {
+    res.type("html")
+    res.end(ruruHTML({ endpoint: "/graphql" }))
+  })
